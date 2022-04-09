aeda9ec7f3d6380f7540a80ecd53828141733623
vincent <baidem@gmail.com>
2022-04-09 13:26:55 +0200

    SockService

--- src/main/java/fr/baidem/tiroir/service/SockService.java
+++ src/main/java/fr/baidem/tiroir/service/SockService.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.baidem.tiroir.entity.Sock;
 import fr.baidem.tiroir.repository.SockRepository;
 import fr.baidem.tiroir.service.dto.sock.CreateSockDTO;
+import fr.baidem.tiroir.service.dto.sock.SockDTO;
 
 @Service
 public class SockService {
@@ -52,11 +54,22 @@ public class SockService {
 		return sockRepository.findBySeason(season);
 	}
 	
+	@Transactional
 	public void create(CreateSockDTO dto) {
 		Sock sock = new Sock(dto.getBrand(), dto.getColor(), dto.getSize(), dto.getStyle(), dto.getSeason());
 		sockRepository.save(sock);
 	}
 	
+	@Transactional
+	public SockDTO createDTO(CreateSockDTO creationDTO) {
+		Sock sock = new Sock(creationDTO.getBrand(), creationDTO.getColor(), creationDTO.getSize(), creationDTO.getStyle(), creationDTO.getSeason());
+		sockRepository.save(sock);
+		
+		SockDTO dto = new SockDTO();
 
+        BeanUtils.copyProperties(sock, dto);
+
+        return dto;
+	}
 
 }
