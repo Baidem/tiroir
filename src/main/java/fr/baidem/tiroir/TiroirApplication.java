c5669a58e2617a8355176accc3c1d0f6d34f6852
vincent <baidem@gmail.com>
2022-04-16 18:31:16 +0200

    Tiroir application : séquence de création de chaussettes

--- src/main/java/fr/baidem/tiroir/TiroirApplication.java
+++ src/main/java/fr/baidem/tiroir/TiroirApplication.java
@@ -41,6 +41,8 @@ public class TiroirApplication {
 		List<Drawer> drawers = drawerService.findAll();
 		List<Sock> socks = sockService.findAll();
 		Drawer topDrawer = null;
+		Drawer bottomDrawer = null;
+
 		
 
 		System.out.println(drawers.size() + " drawer find");
@@ -60,19 +62,82 @@ public class TiroirApplication {
 				}
 				if (drawer.getName().equals("bottom drawer")) {
 					System.out.println(drawers.size() + " drawer(s) find," + " bottom drawer created");
+					bottomDrawer = drawer;
 				}
 			}
 
-			CreateSockDTO dtoSock1 = new CreateSockDTO("sosocks", "red", 42, Style.SPORT_SHORT, Season.SUMMER);
+			CreateSockDTO dtoSock1 = new CreateSockDTO("sosocks", "blue", 42, Style.CITY_LONG, Season.SPRING);
 			this.sockService.create(dtoSock1);
+			
+			CreateSockDTO dtoSock2 = new CreateSockDTO("sosocks", "green", 42, Style.CITY_LONG, Season.SPRING);
+			this.sockService.create(dtoSock2);
+			
+			CreateSockDTO dtoSock3 = new CreateSockDTO("sosocks", "yellow", 42, Style.CITY_LONG, Season.SPRING);
+			this.sockService.create(dtoSock3);
+			
+			CreateSockDTO dtoSock4 = new CreateSockDTO("sosocks", "red", 42, Style.SPORT_LONG, Season.WINTER);
+			this.sockService.create(dtoSock4);
+			
+			CreateSockDTO dtoSock5 = new CreateSockDTO("dim", "blue", 42, Style.CITY_SHORT, Season.SUMMER);
+			this.sockService.create(dtoSock5);
+			
+			CreateSockDTO dtoSock6 = new CreateSockDTO("dim", "green", 42, Style.CITY_SHORT, Season.SUMMER);
+			this.sockService.create(dtoSock6);
+			
+			CreateSockDTO dtoSock7 = new CreateSockDTO("dim", "yellow", 42, Style.SPORT_LONG, Season.AUTUMN);
+			this.sockService.create(dtoSock7);
+			
+			CreateSockDTO dtoSock8 = new CreateSockDTO("dim", "red", 42, Style.CITY_SHORT, Season.SUMMER);
+			this.sockService.create(dtoSock8);
 
 			socks = sockService.findAll();
 			for (Sock sock : socks) {
-				if (sock.getBrand().equals("sosocks")) {
-					System.out.println(socks.size() + " some sock find," + " red sosocks created");
+				if (sock.getBrand().equals("sosocks") && sock.getColor().equals("blue")) {
 					sock.setDrawer(topDrawer);
 					this.sockRepository.save(sock);
-
+					System.out.println("blue sosocks created in the top drawer");
+				}
+				
+				if (sock.getBrand().equals("sosocks") && sock.getColor().equals("green")) {
+					sock.setDrawer(topDrawer);
+					this.sockRepository.save(sock);
+					System.out.println("green sosocks created in the top drawer");
+				}
+				
+				if (sock.getBrand().equals("sosocks") && sock.getColor().equals("yellow")) {
+					sock.setDrawer(topDrawer);
+					this.sockRepository.save(sock);
+					System.out.println("yellow sosocks created in the top drawer");
+				}
+				
+				if (sock.getBrand().equals("sosocks") && sock.getColor().equals("red")) {
+					sock.setDrawer(topDrawer);
+					this.sockRepository.save(sock);
+					System.out.println("red sosocks created in the top drawer");
+				}
+				
+				if (sock.getBrand().equals("dim") && sock.getColor().equals("blue")) {
+					sock.setDrawer(bottomDrawer);
+					this.sockRepository.save(sock);
+					System.out.println("blue dim created in the bottom drawer");
+				}
+				
+				if (sock.getBrand().equals("dim") && sock.getColor().equals("green")) {
+					sock.setDrawer(bottomDrawer);
+					this.sockRepository.save(sock);
+					System.out.println("green dim created in the bottom drawer");
+				}
+				
+				if (sock.getBrand().equals("dim") && sock.getColor().equals("yellow")) {
+					sock.setDrawer(bottomDrawer);
+					this.sockRepository.save(sock);
+					System.out.println("yellow dim created in the bottom drawer");
+				}
+				
+				if (sock.getBrand().equals("dim") && sock.getColor().equals("red")) {
+					sock.setDrawer(bottomDrawer);
+					this.sockRepository.save(sock);
+					System.out.println("red dim created in the bottom drawer");
 				}
 			}
 		}
